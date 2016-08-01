2a01221b6219a60f67105c858f462e55f3fc8e7c
Shaienn <shaienn@mail.ru>
2016-08-01 20:07:46 +0300

    Last changes from hackaton

--- client.c
+++ client.c
@@ -40,7 +40,7 @@ static int is_login_shell = 0;
 
 static void sighandler(int signum) {
     if (signum == SIGCHLD) {
-        got_sigchld = 1;
+	got_sigchld = 1;
     }
 }
 
@@ -52,8 +52,8 @@ static int setup_terminal(int tty, struct termios *tm) {
 
     ret = tcgetattr(0, tm);
     if (ret) {
-        client_err("Failed to get terminal attributes: %d\n", errno);
-        return -1;
+	client_err("Failed to get terminal attributes: %d\n", errno);
+	return -1;
     }
 
     memcpy(&tbttr, tm, sizeof (tbttr));
@@ -83,8 +83,8 @@ static int create_server_connection(Connection_Data_t *cd) {
     assert(cd != NULL);
 
     if ((cd->server_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
-        client_err("Failed to open socket: %d\n", errno);
-        return -1;
+	client_err("Failed to open socket: %d\n", errno);
+	return -1;
     }
 
     cd->server_addr.sin_family = AF_INET;
@@ -96,7 +96,7 @@ static int create_server_connection(Connection_Data_t *cd) {
 }
 
 static int create_real_shell_run_cmd(char *argv[],
-        char *cmd_str, int cmd_str_max_length) {
+	char *cmd_str, int cmd_str_max_length) {
 
     int cmd_str_index = 0;
     char *last_slash = NULL;
@@ -106,45 +106,45 @@ static int create_real_shell_run_cmd(char *argv[],
     assert(argv != NULL);
 
     if ((last_slash = strrchr(argv[0], '/')) == NULL) {
-        last_slash = argv[0];
+	last_slash = argv[0];
     } else {
-        last_slash++;
+	last_slash++;
     }
     if ('-' == (*last_slash)) {
-        is_login_shell = 1;
-        last_slash++;
+	is_login_shell = 1;
+	last_slash++;
     }
 
     client_dbg("CMD: %s\n", last_slash);
 
     if (n > cmd_str_max_length - cmd_str_index)
-        goto not_enough_buffer_length;
+	goto not_enough_buffer_length;
 
 
     memcpy(&cmd_str[cmd_str_index], REAL_SHELL_DIR, n);
     cmd_str_index += n;
 
     if (cmd_str[cmd_str_index - 1] != '/') {
-        cmd_str[cmd_str_index++] = '/';
+	cmd_str[cmd_str_index++] = '/';
     }
 
     n = strlen(last_slash);
 
     if (n > cmd_str_max_length - cmd_str_index)
-        goto not_enough_buffer_length;
+	goto not_enough_buffer_length;
 
     memcpy(&cmd_str[cmd_str_index], last_slash, n);
     cmd_str_index += n;
 
     n = 1;
     if (n > cmd_str_max_length - cmd_str_index)
-        goto not_enough_buffer_length;
+	goto not_enough_buffer_length;
     cmd_str[cmd_str_index++] = '\0';
 
     return 0;
 not_enough_buffer_length:
     client_err("Not enough buffer length for real shell command [need: %d, available: %d]\n",
-            n, cmd_str_max_length - cmd_str_index);
+	    n, cmd_str_max_length - cmd_str_index);
     return -1;
 }
 
@@ -165,7 +165,7 @@ static int encrypt_and_send(Connection_Data_t *cd, Session_Data_t * sd) {
     /* setup a new RC4 IV */
 
     for (rc4_offset = 0; rc4_offset < RC4_SZ; rc4_offset++)
-        msg[rc4_offset] = rand() & 0xFF;
+	msg[rc4_offset] = rand() & 0xFF;
 
     /* append the packet header */
 
@@ -195,8 +195,8 @@ static int encrypt_and_send(Connection_Data_t *cd, Session_Data_t * sd) {
     sha1_finish(&sha1, &msg[RC4_SZ + msg_index]);
 
     sendto(cd->server_fd, msg, RC4_SZ + msg_index + SHA1_SZ, 0,
-            (struct sockaddr *) &cd->server_addr,
-            sizeof ( cd->server_addr));
+	    (struct sockaddr *) &cd->server_addr,
+	    sizeof ( cd->server_addr));
 
 #ifdef DEBUG
     unsigned char sha1sum[SHA1_SZ];
@@ -218,7 +218,7 @@ static int encrypt_and_send(Connection_Data_t *cd, Session_Data_t * sd) {
     sha1_finish(&sha1, sha1sum);
 
     if (memcmp(sha1sum, msg + test_len - SHA1_SZ, SHA1_SZ) != 0) {
-        shellog_err("SHA-1 checksum verification failed\n");
+	shellog_err("SHA-1 checksum verification failed\n");
     }
 
     memcpy(pktkey, secret, RC4_SZ);
@@ -259,7 +259,7 @@ int main(int argc, char *argv[]) {
     struct termios tty_tm; /* stored initial terminal settings */
     struct termios pty_tm; /* current PTY terminal settings */
     struct timeval tv;
-    char* login_argv[] = { "-l" };
+    char* login_argv[] = {"-l"};
     char** shell_argv = NULL;
 
     cd.server_fd = pty = tty = -1;
@@ -267,41 +267,41 @@ int main(int argc, char *argv[]) {
     /* reconstruct the original shell location */
 
     if (create_real_shell_run_cmd(argv, real_shell, SZARR(real_shell))) {
-        client_err("Create real shell command failed\r\n");
-        goto exec_real_shell;
+	client_err("Create real shell command failed\r\n");
+	goto exec_real_shell;
     }
 
     client_dbg("Start as : %s\r\n", real_shell);
 
     if (isatty(0) == 0 ||
-            isatty(1) == 0 ||
-            isatty(2) == 0 ||
-            getlogin() == NULL) {
+	    isatty(1) == 0 ||
+	    isatty(2) == 0 ||
+	    getlogin() == NULL) {
 
-        client_err("Skipping non interactive shell session\r\n");
-        goto exec_real_shell;
+	client_err("Skipping non interactive shell session\r\n");
+	goto exec_real_shell;
     }
 
     if (create_server_connection(&cd)) {
-        client_err("Create server connection failed\r\n");
-        goto exec_real_shell;
+	client_err("Create server connection failed\r\n");
+	goto exec_real_shell;
     }
 
     /* create a pseudo-terminal */
 
     if (openpty(&pty, &tty, NULL, NULL, NULL) < 0) {
-        client_err("Open pseudo terminal failed: %d\r\n", errno);
-        goto exec_real_shell;
+	client_err("Open pseudo terminal failed: %d\r\n", errno);
+	goto exec_real_shell;
     }
 
     if (ttyname(tty) == NULL) {
-        client_err("Get TTY name failed: %d\r\n", errno);
-        goto exec_real_shell;
+	client_err("Get TTY name failed: %d\r\n", errno);
+	goto exec_real_shell;
     }
 
     if (setup_terminal(tty, &tty_tm)) {
-        client_err("Setup terminal failed\r\n");
-        goto exec_real_shell;
+	client_err("Setup terminal failed\r\n");
+	goto exec_real_shell;
     }
 
     /* handle child exit */
@@ -311,161 +311,159 @@ int main(int argc, char *argv[]) {
     /* fork to exec the original shell */
 
     switch ((sd.pid = fork())) {
-        case(-1):
-            reset_terminal(&tty_tm);
-            client_err("Fork process failed: %d\r\n", errno);
-            goto exec_real_shell;
-            break;
-        case(0):
+	case(-1):
+	    reset_terminal(&tty_tm);
+	    client_err("Fork process failed: %d\r\n", errno);
+	    goto exec_real_shell;
+	    break;
+	case(0):
 
-            /* Child : real shell */
+	    /* Child : real shell */
 
-            if (login_tty(tty)) {
-                client_err("Make tty be the controlling terminal failed: %d\r\n", errno);
-                goto exec_real_shell;
-            }
+	    if (login_tty(tty)) {
+		client_err("Make tty be the controlling terminal failed: %d\r\n", errno);
+		goto exec_real_shell;
+	    }
 
-            close(cd.server_fd);
+	    close(cd.server_fd);
 
-            usleep(50000);
-            if (is_login_shell) {
-                shell_argv = login_argv;
-            } else {
-                shell_argv = argv;
-            }
-            execv(real_shell, shell_argv);
-            tcsetattr(0, TCSANOW, &tty_tm);
-            exit(1);
-
-
-            break;
-        default:
-
-            /* Parent : shell logger */
+	    usleep(50000);
+	    if (is_login_shell) {
+		shell_argv = login_argv;
+	    } else {
+		shell_argv = argv;
+	    }
+	    execv(real_shell, shell_argv);
+	    tcsetattr(0, TCSANOW, &tty_tm);
+	    exit(1);
 
 
-            sd.uid = getuid();
+	    break;
+	default:
 
-            while (1) {
-
-                FD_ZERO(&rd);
-                FD_SET(pty, &rd);
-                FD_SET(0, &rd);
-
-                int n = (pty > 0) ? pty : 0;
-
-                if ((n = select(n + 1, &rd, NULL, NULL, NULL)) < 0) {
-                    if (n < 0 && (errno == EINTR || errno == EAGAIN))
-                        goto check_sigs;
-                    break;
-                }
-
-                if (FD_ISSET(0, &rd)) {
-
-                    gettimeofday(&tv, NULL);
-                    sd.time = tv.tv_sec;
-
-                    /* transfer the data from stdin to pty */
-
-                    if ((n = read(0, sd.buffer, BUF_SZ)) <= 0) {
-                        if (n < 0 && (errno == EINTR || errno == EAGAIN))
-                            goto check_sigs;
-                        break;
-                    }
-
-                    /* Check target terminal ECHO flag to skip password typing */
-
-                    if (tcgetattr(pty, &pty_tm)) {
-                        client_err("Failed to get terminal attributes: %d\r\n", errno);
-                        goto check_sigs;
-                    }
-
-                    struct termios prev_pty_tm; /* current PTY terminal settings */
-                    if (memcmp(&pty_tm, &prev_pty_tm, sizeof (struct termios))) {
-
-                        printf("c_iflag\r\n");
-                        printf(" IGNBRK %d\r\n", pty_tm.c_iflag & IGNBRK);
-                        printf(" BRKINT %d\r\n", pty_tm.c_iflag & BRKINT);
-                        printf(" IGNPAR %d\r\n", pty_tm.c_iflag & IGNPAR);
-                        printf(" PARMRK %d\r\n", pty_tm.c_iflag & PARMRK);
-                        printf("  INPCK %d\r\n", pty_tm.c_iflag & INPCK);
-                        printf(" ISTRIP %d\r\n", pty_tm.c_iflag & ISTRIP);
-                        printf("  INLCR %d\r\n", pty_tm.c_iflag & INLCR);
-                        printf("  IGNCR %d\r\n", pty_tm.c_iflag & IGNCR);
-                        printf("  ICRNL %d\r\n", pty_tm.c_iflag & ICRNL);
-                        printf("  IUCLC %d\r\n", pty_tm.c_iflag & IUCLC);
-                        printf("   IXON %d\r\n", pty_tm.c_iflag & IXON);
-                        printf("  IXANY %d\r\n", pty_tm.c_iflag & IXANY);
-                        printf("  IXOFF %d\r\n", pty_tm.c_iflag & IXOFF);
-                        printf("IMAXBEL %d\r\n", pty_tm.c_iflag & IMAXBEL);
-                        printf("  IUTF8 %d\r\n", pty_tm.c_iflag & IUTF8);
-
-                        memcpy(&prev_pty_tm, &pty_tm, sizeof (struct termios));
-                    }
+	    /* Parent : shell logger */
 
 
-                    sd.dir = INPUT_DIR;
-                    sd.len = n;
-                    encrypt_and_send(&cd, &sd);
+	    sd.uid = getuid();
 
+	    while (1) {
 
-                    if ((n = write(pty, sd.buffer, n)) != n) {
-                        if (n < 0 && (errno == EINTR || errno == EAGAIN))
-                            goto check_sigs;
+		FD_ZERO(&rd);
+		FD_SET(pty, &rd);
+		FD_SET(0, &rd);
 
-                        break;
-                    }
-                }
+		int n = (pty > 0) ? pty : 0;
 
-                if (FD_ISSET(pty, &rd)) {
+		if ((n = select(n + 1, &rd, NULL, NULL, NULL)) < 0) {
+		    if (n < 0 && (errno == EINTR || errno == EAGAIN))
+			goto check_sigs;
+		    break;
+		}
 
-                    gettimeofday(&tv, NULL);
-                    sd.time = tv.tv_sec;
+		if (FD_ISSET(0, &rd)) {
 
-                    /* transfer the data from pty to stdout */
+		    gettimeofday(&tv, NULL);
+		    sd.time = tv.tv_sec;
 
-                    if ((n = read(pty, sd.buffer, BUF_SZ)) <= 0) {
-                        if (n < 0 && (errno == EINTR || errno == EAGAIN))
-                            goto check_sigs;
+		    /* transfer the data from stdin to pty */
 
-                        break;
-                    }
+		    if ((n = read(0, sd.buffer, BUF_SZ)) <= 0) {
+			if (n < 0 && (errno == EINTR || errno == EAGAIN))
+			    goto check_sigs;
+			break;
+		    }
+
+		    /* Check target terminal ECHO flag to skip password typing */
+
+		    if (tcgetattr(pty, &pty_tm)) {
+			client_err("Failed to get terminal attributes: %d\r\n", errno);
+			goto check_sigs;
+		    }
+
+		    struct termios prev_pty_tm; /* current PTY terminal settings */
+		    if (memcmp(&pty_tm, &prev_pty_tm, sizeof (struct termios))) {
+
+			printf("c_iflag\r\n");
+			printf(" IGNBRK %d\r\n", pty_tm.c_iflag & IGNBRK);
+			printf(" BRKINT %d\r\n", pty_tm.c_iflag & BRKINT);
+			printf(" IGNPAR %d\r\n", pty_tm.c_iflag & IGNPAR);
+			printf(" PARMRK %d\r\n", pty_tm.c_iflag & PARMRK);
+			printf("  INPCK %d\r\n", pty_tm.c_iflag & INPCK);
+			printf(" ISTRIP %d\r\n", pty_tm.c_iflag & ISTRIP);
+			printf("  INLCR %d\r\n", pty_tm.c_iflag & INLCR);
+			printf("  IGNCR %d\r\n", pty_tm.c_iflag & IGNCR);
+			printf("  ICRNL %d\r\n", pty_tm.c_iflag & ICRNL);
+			printf("  IUCLC %d\r\n", pty_tm.c_iflag & IUCLC);
+			printf("   IXON %d\r\n", pty_tm.c_iflag & IXON);
+			printf("  IXANY %d\r\n", pty_tm.c_iflag & IXANY);
+			printf("  IXOFF %d\r\n", pty_tm.c_iflag & IXOFF);
+			printf("IMAXBEL %d\r\n", pty_tm.c_iflag & IMAXBEL);
+			printf("  IUTF8 %d\r\n", pty_tm.c_iflag & IUTF8);
+
+			memcpy(&prev_pty_tm, &pty_tm, sizeof (struct termios));
+		    }
+
+		    sd.dir = INPUT_DIR;
+		    sd.len = n;
+		    encrypt_and_send(&cd, &sd);
+
+		    if ((n = write(pty, sd.buffer, n)) != n) {
+			if (n < 0 && (errno == EINTR || errno == EAGAIN))
+			    goto check_sigs;
+
+			break;
+		    }
+		}
+
+		if (FD_ISSET(pty, &rd)) {
+
+		    gettimeofday(&tv, NULL);
+		    sd.time = tv.tv_sec;
+
+		    /* transfer the data from pty to stdout */
+
+		    if ((n = read(pty, sd.buffer, BUF_SZ)) <= 0) {
+			if (n < 0 && (errno == EINTR || errno == EAGAIN))
+			    goto check_sigs;
+
+			break;
+		    }
 
 #if LOG_OUTPUT
-                    sd.dir = OUTPUT_DIR;
-                    sd.len = n;
-                    encrypt_and_send(&cd, &sd);
+		    sd.dir = OUTPUT_DIR;
+		    sd.len = n;
+		    encrypt_and_send(&cd, &sd);
 #endif
-                    if ((n = write(1, sd.buffer, n)) != n) {
-                        if (n < 0 && (errno == EINTR || errno == EAGAIN))
-                            goto check_sigs;
+		    if ((n = write(1, sd.buffer, n)) != n) {
+			if (n < 0 && (errno == EINTR || errno == EAGAIN))
+			    goto check_sigs;
 
-                        break;
-                    }
-                }
+			break;
+		    }
+		}
 
 check_sigs:
-                if (got_sigchld == 1) {
-                    got_sigchld = 0;
-                    goto super_exit;
-                    break;
-                }
+		if (got_sigchld == 1) {
+		    got_sigchld = 0;
+		    goto super_exit;
+		    break;
+		}
 
-            }
+	    }
 
-            break;
+	    break;
     }
 
 exec_real_shell:
 
     if (tty >= 0)
-        close(tty);
+	close(tty);
 
     if (pty >= 0)
-        close(pty);
+	close(pty);
 
     if (cd.server_fd >= 0)
-        close(cd.server_fd);
+	close(cd.server_fd);
 
 
     client_dbg("exec_before_exit, parent: %zu; me: %zu\n", getppid(), getpid());
